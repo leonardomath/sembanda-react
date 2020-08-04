2195158b96e293b7ec4a3c21fa80eabe00219e23
Leonardo Matheus <lleomath@gmail.com>
2020-08-04 10:21:36 -0300

    - SignUp page with new design

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -4,6 +4,12 @@ import NavBar from '../components/NavBar'
 import Radio from '../components/InputRadio'
 
 import './style.css'
+import AvatarUser from '../components/AvatarUser'
+
+function previewImage(e) {
+  const file = document.querySelector('#file')
+  document.querySelector('.avatar').src = window.URL.createObjectURL(file.files[0])
+}
 
 const index = () => {
   return (
@@ -15,16 +21,23 @@ const index = () => {
         </div>
         <div className="form-signup">
           <h1 className="form-signup-title">Nova conta</h1>
-          <form method="post">
+          <form method="post" encType="form/data">
+            <label htmlFor="file">
+              <AvatarUser />
+            </label>
+            <input id="file" type="file" onChange={previewImage} />
             <input type="text" placeholder="Nome" />
             <input type="email" placeholder="E-mail" />
             <input type="password" placeholder="Senha" />
-            <Radio title="Guitarra" />
-            <Radio title="Violão" />
-            <Radio title="Baixo" />
-            <Radio title="Bateria" />
-            <Radio title="Teclado" />
-            <Radio title="Vocal" />
+            <select>
+              <option value="" selected disabled>Escolha seu instrumento</option>
+              <option value="Guitarra">Guitarra</option>
+              <option value="Violão">Violão</option>
+              <option value="Baixo">Baixo</option>
+              <option value="Bateria">Bateria</option>
+              <option value="Teclado">Teclado</option>
+              <option value="Vocal">Vocal</option>
+            </select>
             <button type="submit">Cadastrar-se</button>
           </form>
         </div>
