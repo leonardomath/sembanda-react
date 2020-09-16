fb4d9f1aa09f1ff3596dd623364c05e09ef4094a
Leonardo Matheus <lleomath@gmail.com>
2020-09-16 18:45:00 -0300

    changed the album  search title

--- src/pages/Album/index.js
+++ src/pages/Album/index.js
@@ -82,7 +82,7 @@ export default function Album() {
         <FaPlus className="IconPlus" size={18} color="#000" />
       </button>
       <h1>Album disponíveis</h1>
-      <Search info="Procurar album" band={band} setBand={setBand} />
+      <Search info="Procurar banda" band={band} setBand={setBand} />
       <section className="album-list">
         {albuns
           ? albuns.map((album, key) => (
