4f5cae9842e1c40978ccf58dc4d4d42872f34f2b
Leonardo Matheus <lleomath@gmail.com>
2020-09-19 16:36:11 -0300

    custom audio player added

--- src/pages/components/AudioPlayer/index.js
+++ src/pages/components/AudioPlayer/index.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import './style.css'
 import api from '../../../services/api';
+import { Media, Player, controls, } from "react-media-player";
+const {
+  PlayPause,
+  CurrentTime,
+  SeekBar,
+} = controls;
 
 export default function AudioPlayer({url,title,id, setLoading}) {
-
   async function removeSong(id) {
     setLoading(true)
     try {
@@ -20,11 +25,20 @@ export default function AudioPlayer({url,title,id, setLoading}) {
   }
   return (
     <div className="audio-player">
-      <button onClick={() => removeSong(id)}>x</button>
+      <button className="close" onClick={() => removeSong(id)}>x</button>
       <h1>{title}</h1>
-      <audio controls>
-        <source src={url} type="audio/mpeg" />
-      </audio>
+      <Media>
+        <div className="media">
+          <div className="media-player">
+            <Player src={url} />
+          </div>
+          <div className="media-controls">
+            <PlayPause />
+            <CurrentTime />
+            <SeekBar />
+          </div>
+        </div>
+      </Media>
     </div>
   );
 }
